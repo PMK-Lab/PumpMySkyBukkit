56e97a1196cd45040eba9a1c84f5964497cf5264
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-12-08 21:14:23 +0100

    Create class BukkitIslandManager & extends it from abstract core

---
+++ pumpmyskybukkit/src/main/java/fr/pumpmyskybukkit/BukkitIslandManager.java
@@ -0,0 +1,57 @@
+package fr.pumpmyskybukkit;
+
+import org.bukkit.entity.Player;
+
+import fr.pumpmyskycore.Island;
+import fr.pumpmyskycore.IslandManager;
+
+public class BukkitIslandManager extends IslandManager<Player>{
+
+	protected MainSky main;
+	
+	public BukkitIslandManager(MainSky main) {
+		super(main.getDataFolder().toPath());
+		
+		this.main = main;
+		
+	}
+
+	@Override
+	public Island createIsland(Player player) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Island playerGetIsland(Player player) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void playerSetIsland(Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void playerUnsetIsland(Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean playerHasIsland(Player player) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void deleteIsland(Island i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
