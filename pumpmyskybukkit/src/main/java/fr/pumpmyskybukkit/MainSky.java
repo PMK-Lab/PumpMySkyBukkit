44dd5697fe1b9584f3ffef391761c81473267dd2
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-12-08 15:33:10 +0100

    Extends class from JavaPlugin & add override methods

--- pumpmyskybukkit/src/main/java/fr/pumpmyskybukkit/MainSky.java
+++ pumpmyskybukkit/src/main/java/fr/pumpmyskybukkit/MainSky.java
@@ -1,5 +1,23 @@
 package fr.pumpmyskybukkit;
 
-public class MainSky {
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class MainSky extends JavaPlugin{
+
+	@Override
+	public void onEnable() {
+		super.onEnable();
+		
+		
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		super.onDisable();
+		
+		
+		
+	}
+	
 }
