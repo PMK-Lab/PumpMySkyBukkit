e62b2807ef71875b6d8ecd9ea8bcaf02bc65f583
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-12-08 21:20:07 +0100

    Create class SkyListener & add constructor/field

---
+++ pumpmyskybukkit/src/main/java/fr/pumpmyskybukkit/SkyListeners.java
@@ -0,0 +1,17 @@
+package fr.pumpmyskybukkit;
+
+import org.bukkit.event.Listener;
+
+public class SkyListeners implements Listener {
+
+	private BukkitIslandManager manager;
+	
+	public SkyListeners(BukkitIslandManager islandManager) {
+		
+		this.manager = islandManager;
+		
+	}
+	
+	
+
+}
