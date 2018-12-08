fbf0894a83ed55aa19797f82091f66798e42e816
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-12-09 00:04:42 +0100

    Add class sub command JoinIsland

---
+++ pumpmyskybukkit/src/main/java/fr/pumpmyskybukkit/commands/JoinIslandCmd.java
@@ -0,0 +1,157 @@
+package fr.pumpmyskybukkit.commands;
+
+import java.util.List;
+import org.bukkit.command.Command;
+import org.bukkit.entity.Player;
+
+public class JoinIslandCmd implements ISubCommand {
+
+	@Override
+	public boolean onSubCommand(Player sender, Command cmd, List<String> args) {
+		return false;
+		// TODO Auto-generated method stub
+		/*
+		IslandManager is = MainOzone.getIslandManager();
+		
+		if(!is.playerHasIsland(sender)) {
+			
+			File f = new File(MainOzone.getInstance().getDataFolder(),"islands_invite.yml");
+			YamlConfiguration y = MainOzone.getConf().getConfiguration(f);
+			
+			if(!y.contains("invite." + sender.getUniqueId().toString())) {
+				y.set("invite." + sender.getUniqueId().toString(), new ArrayList<String>());
+			}
+			
+			List<String> l = y.getStringList("invite." + sender.getUniqueId().toString());
+			
+			if(args.isEmpty()) {
+				
+				aide2(sender,l);
+				return true;
+				
+			}else {
+				
+				String u = args.get(0);
+				
+				if(!l.contains(u)) {
+					
+					aide1(sender);
+					return false;
+					
+				}else {
+					
+					// ajout à l'ile		
+					try {
+						String a = args.get(1);
+						
+						if(a.equals("-a")) {
+							
+							for (Iterator<String> i = l.iterator(); i.hasNext();) {
+								
+								if(i.next().equals(u)) {
+									
+									i.remove();
+									
+									OfflinePlayer owner = MainOzone.getInstance().getServer().getOfflinePlayer(UUID.fromString(u));
+									
+									Island island = MainOzone.getIslandManager().getIsland(owner);
+									
+									IslandFileData isfd = island.getIslandData();
+									
+									island.addPlayer(sender);
+									island.save();
+									
+									IslandManager.setIsland(sender, isfd);
+									
+									y.set("invite." + sender.getUniqueId().toString(), l);
+									y.save(f);
+									
+									sender.sendMessage(Island.prefix + "§r§d Vous avez accepté l'invitation vers l'île " + owner.getName());
+									new CreateIslandCmd().aide1(sender);
+									
+									break;
+									
+								}
+								
+							}
+							
+						}else {
+							// remove de l'invitation
+							
+							for (Iterator<String> i = l.iterator(); i.hasNext();) {
+								
+								if(i.next().equals(u)) {
+									
+									i.remove();
+									y.set("invite." + sender.getUniqueId().toString(), l);
+									y.save(f);
+									sender.sendMessage(Island.prefix + "§r§c Invitation suprimée !");
+									break;
+									
+								}
+								
+							}
+							
+						}
+						
+					} catch (Exception e) {
+						// TODO: handle exception
+						e.printStackTrace();
+					}					
+									
+					return true;
+					
+				}
+				
+			}
+				
+		}else {
+			// affichage aide
+			new CreateIslandCmd().aide(sender);
+			return true;
+		}
+		*/
+	}
+/*
+	public void aide2(Player p, List<String> l) {
+		// TODO Auto-generated method stub
+		
+		p.sendMessage(Island.prefix + "§r§d Liste des invitations :");
+		
+		for (String string : l) {
+			
+			OfflinePlayer t = MainOzone.getInstance().getServer().getOfflinePlayer(UUID.fromString(string));
+			
+			TextComponent accept = new TextComponent("§l§2[√]");
+			accept.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/is join " + string + " -a"));
+			accept.setHoverEvent( new HoverEvent( HoverEvent.Action.SHOW_TEXT, new ComponentBuilder( "§3§l Accepter l'invitation" ).create() ) );
+			
+			TextComponent refuse = new TextComponent("§c§l[✖]");
+			refuse.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/is join " + string + " -r"));
+			refuse.setHoverEvent( new HoverEvent( HoverEvent.Action.SHOW_TEXT, new ComponentBuilder( "§3§lRefuser l'invitation" ).create() ) );
+		
+			TextComponent msg = new TextComponent("Ile de " + t.getName() + " : ");
+			msg.setBold(true);
+			msg.setColor(ChatColor.AQUA);
+			msg.addExtra(accept);
+			msg.addExtra(refuse);
+			
+			p.spigot().sendMessage(msg);
+		
+		}
+		
+	}
+	
+	public void aide1(Player p) {
+		
+		p.sendMessage(Island.prefix + "§r§c L'invitation n'est pas valide.");
+		
+	}
+
+	@Override
+	public void aide(Player p) {
+		// TODO Auto-generated method stub
+		p.sendMessage(Island.prefix + "§r§c Vous faites déjà parti d'un");
+	}
+*/
+}
