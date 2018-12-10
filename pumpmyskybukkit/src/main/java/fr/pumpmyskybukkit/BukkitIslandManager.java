ed4ce41a5c089bd21e653cf964d89216b2db4a42
Clem-Fern <clement.coussieu@gmail.com>
2018-12-11 00:11:44 +0100

    Update/Fix import in BukkitIslandManager class

--- pumpmyskybukkit/src/main/java/fr/pumpmyskybukkit/BukkitIslandManager.java
+++ pumpmyskybukkit/src/main/java/fr/pumpmyskybukkit/BukkitIslandManager.java
@@ -1,12 +1,12 @@
 package fr.pumpmyskybukkit;
 
 import java.io.IOException;
+import java.util.UUID;
 
 import org.bukkit.entity.Player;
 
 import fr.pumpmyskycore.Island;
 import fr.pumpmyskycore.IslandManager;
-import fr.pumpmyskycore.exceptions.PlayerAlreadyHaveIslandException;
 import fr.pumpmyskycore.exceptions.PlayerDoesNotHaveIslandException;
 import fr.pumpmyskycore.exceptions.PlayerNotInThisIsland;
 
