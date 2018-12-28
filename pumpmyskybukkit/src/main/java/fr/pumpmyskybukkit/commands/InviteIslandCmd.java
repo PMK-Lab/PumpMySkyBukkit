e5288527192140d9dd3c50885d30fa579b61f7a9
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-12-28 01:17:02 +0100

    Remove useless proccess

--- pumpmyskybukkit/src/main/java/fr/pumpmyskybukkit/commands/InviteIslandCmd.java
+++ pumpmyskybukkit/src/main/java/fr/pumpmyskybukkit/commands/InviteIslandCmd.java
@@ -23,75 +23,19 @@ public class InviteIslandCmd implements ISubCommand, TabCompleter {
 	
 	@Override
 	public boolean onSubCommand(IslandCommandExecutor exec, Player sender, Command cmd, List<String> args) {
-		
-		try {
 			
-			Island island = exec.getIslandManager().playerGetIsland(sender);
-			
-			if(!island.getOwner().equals(sender.getUniqueId().toString())) {
-				
-				// error permission island message / vous devez etre propriétaire de l'ile pour faire cela
-				sender.sendMessage(IslandManagerConstant.ISLAND_CHAT_PREFIX + "§r§c Vous devez etre propriétaire de l'ile pour faire cela !");
-				return true;
-				
-			}
-			
-			if(args.isEmpty()) {
-				// show lis of current invite
+			if(args.isEmpty() || args.size() != 1) {
+				// invalide syntax
 				
 				
 				
 			}else {
 				
-				String offlinePlayerString = args.get(0);
 				
-				if(!this.onTabComplete(sender, cmd, null, null).contains(offlinePlayerString) & (offlinePlayerString.startsWith("-") & !this.onTabComplete(sender, cmd, null, null).contains(offlinePlayerString.substring(1)))) {
-					// offline player not found
-					sender.sendMessage(IslandManagerConstant.ISLAND_CHAT_PREFIX +"§c Joueur invalide !");					
-					
-				}else {
-					// offline player found
-					
-					if(offlinePlayerString.startsWith("-")) {
-						// delete invite
-						
-						String offlinePlayerName = offlinePlayerString.substring(1);
-						@SuppressWarnings("deprecation")
-						OfflinePlayer player = this.main.getServer().getOfflinePlayer(offlinePlayerName);
-						
-						//exec.getIslandManager().playerUninviteIslande(player);
-						
-						sender.sendMessage(IslandManagerConstant.ISLAND_CHAT_PREFIX +"§d Invitation annulée !");						
-						
-					}else {
-						// invite player
-						
-						OfflinePlayer player = this.main.getServer().getOfflinePlayer(offlinePlayerString);
-						
-						//exec.getIslandManager().playerInviteIslande(player);
-						
-						sender.sendMessage(IslandManagerConstant.ISLAND_CHAT_PREFIX +"§d Invitation bien envoyé !");
-						
-						if(player.isOnline()) {
-							
-							Player onlinePlayer = player.getPlayer();
-							
-							onlinePlayer.sendMessage(IslandManagerConstant.ISLAND_CHAT_PREFIX +"§d " + sender.getName() + " vous a invité à rejoindre son ile !");
-							
-						}
-						
-					}
-					
-				}
 				
 			}
-			
-		} catch (PlayerDoesNotHaveIslandException e) {
-			
-			sender.sendMessage(IslandManagerConstant.ISLAND_CHAT_PREFIX + "§r§c Vous ne possedez ou ne faite parti d'aucune ile !");
-			new CreateIslandCmd().createIslandChatMessage(sender);
-			
-		}
+				
+				
 		
 		
 		
