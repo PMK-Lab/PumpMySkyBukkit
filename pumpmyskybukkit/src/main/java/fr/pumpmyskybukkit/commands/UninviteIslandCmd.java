f476fe2b05bb2a2d5349874c8c6cb4dbd5f85a4d
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-12-31 16:23:54 +0100

    Add uninvite proccess in UninviteIslandCmd class

--- pumpmyskybukkit/src/main/java/fr/pumpmyskybukkit/commands/UninviteIslandCmd.java
+++ pumpmyskybukkit/src/main/java/fr/pumpmyskybukkit/commands/UninviteIslandCmd.java
@@ -37,6 +37,41 @@ public class UninviteIslandCmd implements ISubCommand, SubTabCompleter {
 			BukkitIslandManager manager = exec.getIslandManager();			
 			OfflinePlayer player = manager.getOfflinePlayerByName(args.get(0));
 			
+			if(player != null) {
+				
+				try {
+					
+					manager.playerUninviteIsland(sender, (Player) player);
+					
+					if(player.isOnline()) {
+						
+						player.getPlayer().sendMessage(IslandManagerConstant.ISLAND_CHAT_PREFIX + "§r§d " + sender.getName() + " a supprimé l'invatitation qu'il vous inventant à rejoindre son ile !");
+						
+					}
+					
+				} catch (PlayerDoesNotHaveIslandException e) {
+					
+					sender.sendMessage(IslandManagerConstant.ISLAND_CHAT_PREFIX + "§r§c Vous ne possedez ou ne faite parti d'aucune ile !");
+					new CreateIslandCmd().createIslandChatMessage(sender);
+					
+				} catch (RestrictActionToOwnerIslandException e) {
+					
+					sender.sendMessage(IslandManagerConstant.ISLAND_CHAT_PREFIX + "§r§c Vous devez être le créateur de l'ile pour faire cela !");
+					
+				} catch (PlayerDoesNotInvited e) {
+					
+					sender.sendMessage(IslandManagerConstant.ISLAND_CHAT_PREFIX + "§r§c Ce joueur n'a pas été invité à rejoindre votre ile !");
+					
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}else {
+				
+				sender.sendMessage(IslandManagerConstant.ISLAND_CHAT_PREFIX + "§r§c Synthaxe invalide : /is uninvite <player>");
+				
+			}			
 			
 		}
 		
