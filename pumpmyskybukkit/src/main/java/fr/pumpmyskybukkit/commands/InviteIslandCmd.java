a04b7005f21e0e54b01bbb6b8741ccab59de8d49
Clem-Fern <clement.coussieu@gmail.com>
2019-01-11 00:30:05 +0100

    Update sending message proccess in InviteIslandCmd class

--- pumpmyskybukkit/src/main/java/fr/pumpmyskybukkit/commands/InviteIslandCmd.java
+++ pumpmyskybukkit/src/main/java/fr/pumpmyskybukkit/commands/InviteIslandCmd.java
@@ -13,6 +13,7 @@ import fr.pumpmyplotcore.exceptions.PlayerAlreadyInvitedPlotException;
 import fr.pumpmyplotcore.exceptions.PlayerDoesNotHavePlotException;
 import fr.pumpmyplotcore.exceptions.RestrictActionToPlotOwnerException;
 import fr.pumpmyskybukkit.BukkitIslandManager;
+import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.TextComponent;
 
 public class InviteIslandCmd implements ISubCommand, SubTabCompleter {
@@ -22,7 +23,7 @@ public class InviteIslandCmd implements ISubCommand, SubTabCompleter {
 			
 		if(args.size() != 1) {
 			// invalide syntax			
-			sender.spigot().sendMessage(new TextComponent(PlotManagerConstant.PLOT_CHAT_PREFIX + "§r§c Synthaxe invalide : /is invite <player>"));						
+			sender.spigot().sendMessage(new ComponentBuilder(PlotManagerConstant.PLOT_CHAT_PREFIX + "§r§c Synthaxe invalide : /is invite <player>").create());						
 			
 		}else {
 			
@@ -34,37 +35,37 @@ public class InviteIslandCmd implements ISubCommand, SubTabCompleter {
 				try {
 					
 					manager.playerInvitePlot(sender,player);
-					sender.spigot().sendMessage(new TextComponent(PlotManagerConstant.PLOT_CHAT_PREFIX +"§d Joueur invité avec succès !"));
+					sender.spigot().sendMessage(new ComponentBuilder(PlotManagerConstant.PLOT_CHAT_PREFIX +"§d Joueur invité avec succès !").create());
 					
 					if(player.isOnline()) {
 						
-						player.getPlayer().spigot().sendMessage(new TextComponent(PlotManagerConstant.PLOT_CHAT_PREFIX + "§r§d " + sender.getName() + " vous a invité à rejoindre son ile !"));
+						player.getPlayer().spigot().sendMessage(new ComponentBuilder(PlotManagerConstant.PLOT_CHAT_PREFIX + "§r§d " + sender.getName() + " vous a invité à rejoindre son ile !").create());
 						
 					}
 					
 				} catch (PlayerDoesNotHavePlotException e) {
 					
-					sender.spigot().sendMessage(new TextComponent(PlotManagerConstant.PLOT_CHAT_PREFIX + "§r§c Vous ne possedez ou ne faite parti d'aucune ile !"));
+					sender.spigot().sendMessage(new ComponentBuilder(PlotManagerConstant.PLOT_CHAT_PREFIX + "§r§c Vous ne possedez ou ne faite parti d'aucune ile !").create());
 					new CreateIslandCmd().createIslandChatMessage(sender);
 					
 				} catch (RestrictActionToPlotOwnerException e) {
 					
-					sender.spigot().sendMessage(new TextComponent(PlotManagerConstant.PLOT_CHAT_PREFIX + "§r§c Vous devez être le créateur de l'lie pour faire cela !"));
+					sender.spigot().sendMessage(new ComponentBuilder(PlotManagerConstant.PLOT_CHAT_PREFIX + "§r§c Vous devez être le créateur de l'lie pour faire cela !").create());
 					
 				} catch (PlayerAlreadyInvitedPlotException e) {
 					
-					sender.spigot().sendMessage(new TextComponent(PlotManagerConstant.PLOT_CHAT_PREFIX + "§r§c Vous avez déjà invité ce joueur à rejoindre votre ile !"));
+					sender.spigot().sendMessage(new ComponentBuilder(PlotManagerConstant.PLOT_CHAT_PREFIX + "§r§c Vous avez déjà invité ce joueur à rejoindre votre ile !").create());
 					
 				} catch (IOException e) {
 					
 					e.printStackTrace();
-					sender.spigot().sendMessage(new TextComponent("§cERROR !!!! Envoyez le message suivant au staff : " + e.getClass().getName() + " || " + e.getMessage()));
+					sender.spigot().sendMessage(new ComponentBuilder("§cERROR !!!! Envoyez le message suivant au staff : " + e.getClass().getName() + " || " + e.getMessage()).create());
 					
 				}
 				
 			}else {
 				
-				sender.spigot().sendMessage(new TextComponent(PlotManagerConstant.PLOT_CHAT_PREFIX + "§r§c Synthaxe invalide : /is invite <player>"));
+				sender.spigot().sendMessage(new ComponentBuilder(PlotManagerConstant.PLOT_CHAT_PREFIX + "§r§c Synthaxe invalide : /is invite <player>").create());
 				
 			}		
 			
