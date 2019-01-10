75d1cc1df354e5fb6f9f45f5b752f4440ac1f820
Clem-Fern <clement.coussieu@gmail.com>
2019-01-10 18:19:33 +0100

    Update import in BukkitIslandManager class

--- pumpmyskybukkit/src/main/java/fr/pumpmyskybukkit/BukkitIslandManager.java
+++ pumpmyskybukkit/src/main/java/fr/pumpmyskybukkit/BukkitIslandManager.java
@@ -1,15 +1,27 @@
 package fr.pumpmyskybukkit;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.configuration.InvalidConfigurationException;
-import org.bukkit.entity.Player;
+
+import com.sk89q.worldedit.CuboidClipboard;
+import com.sk89q.worldedit.EditSession;
+import com.sk89q.worldedit.MaxChangedBlocksException;
+import com.sk89q.worldedit.Vector;
+import com.sk89q.worldedit.WorldEdit;
+import com.sk89q.worldedit.bukkit.BukkitWorld;
+import com.sk89q.worldedit.data.DataException;
+import com.sk89q.worldedit.schematic.SchematicFormat;
 
 import fr.pumpmyplotcore.PlotLocation;
 import fr.pumpmyplotcore.PlotManager;
 
+@SuppressWarnings("deprecation")
 public class BukkitIslandManager extends PlotManager<OfflinePlayer>{
 
 	protected MainSky main;
